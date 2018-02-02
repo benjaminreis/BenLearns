8c1f1686defec08f757c8bfde9adfa0ada0394d8
benjaminreis <benjaminreis@gmail.com>
2018-02-01 21:41:57 -0600

    more initial stuff

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,11 +34,28 @@ namespace BenLearns.Controllers
             return View();
         }
 
-        public IActionResult HackerRank()
+        public ActionResult HackerRank()
         {
-            return View();
+            var model = new ViewModels.HackerRankViewModel();
+            return View(model);
         }
 
+
+        public ActionResult Submit(BenLearns.ViewModels.HackerRankViewModel model)
+        {
+            model.CountElements = int.Parse(model.sCountElements);
+                
+            model.Elements = model.sElements.Split(',').Select(int.Parse).ToList();
+            //TODO BEN do stuff
+
+            model.sResult = "Count: " + model.sCountElements + " Elements: " + model.sElements + " All Your Base are Belong to Us";
+            return View("HackerRank", model);
+        }
+
+        //public ActionResult Submit()
+        //{
+        //    return View();
+        //}
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ ViewModels/HackerRankViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BenLearns.ViewModels
+{
+    public class HackerRankViewModel
+    {
+
+        public int CountElements { get; set; }
+        public string sCountElements { get; set; }
+        public List<int> Elements { get; set; }
+        public string sElements { get; set; }
+        public string sResult { get; set; }
+    }
+
+
+}
