f653de659b5ea88fa6e7c0bc4c9bcdf37db9ae0c
benjaminreis <benjaminreis@gmail.com>
2018-06-16 21:02:50 -0500

    a little more react tutorial...  Travel day, didnt have a lot of time to play.  :-(

--- wwwroot/js/Greeting.jsx
+++ wwwroot/js/Greeting.jsx
@@ -1,12 +1,38 @@
-﻿class CommentBox extends React.Component {
+﻿class CommentList extends React.Component {
   render() {
     return (
-      <div className="commentBox">
-        Welcome to the Volunteer App!  This is rendered within React.JS!
+      <div className="commentList">
+        Hello, world! I am a CommentList.
       </div>
     );
   }
 }
+
+class CommentForm extends React.Component {
+  render() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+}
+
+class CommentBox extends React.Component {
+  render() {
+    return (
+      <div className="commentBox">
+        Welcome to the Volunteer App!  This is rendered within React.JS!
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+
+    );
+  }
+}
+
+
 
 ReactDOM.render(
   <CommentBox />,
