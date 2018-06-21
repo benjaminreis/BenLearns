710a0e7dac8a0cec81d68d5de47e6ac3a3c30c97
benjaminreis <benjaminreis@gmail.com>
2018-06-20 19:04:28 -0600

    ugh

--- wwwroot/js/Greeting.jsx
+++ wwwroot/js/Greeting.jsx
@@ -1,6 +1,56 @@
 ﻿
 
 
+
+
+
+
+
+
+class CommentForm extends React.Component {
+  render() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+}
+
+
+
+
+
+class CommentBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
+  loadCommentsFromServer(parentComponent) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('get', this.props.url, true);
+    xhr.onload = () => {
+      const data = JSON.parse(xhr.responseText);
+      parentComponent.setState({ data: data });
+    };
+    xhr.send();
+  }
+  componentDidMount() {
+    this.loadCommentsFromServer(this);
+    window.setInterval(() => this.loadCommentsFromServer(this), this.props.pollInterval);
+  }
+  render() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm />
+      </div>
+    );
+  }
+}
+
+
 class CommentList extends React.Component {
   render() {
       var commentNodes = this.props.data.map(comment => (
@@ -36,72 +86,7 @@ class Comment extends React.Component {
   }
 }
 
-
-
-
-
-class CommentForm extends React.Component {
-  render() {
-    return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
-    );
-  }
-}
-
-
-
-
-
-class CommentBox extends React.Component {
-  //var th = this;
-
-  constructor(props) {
-    super(props);
-    this.state = {data: []};
-  }
-
-  loadCommentsFromServer() {
-
-    const xhr = new XMLHttpRequest();
-    xhr.open('get', this.props.url, true);
-    xhr.onload = () => {
-      const data = JSON.parse(xhr.responseText);
-      //this.setState({ data: data });
-
-      //if (this && this._isMounted) {
-      //TODO BEN this is a problem, the component isnt mounted, so the state never gets set
-         //th.setState({ data: data });
-       //  th.state.data = data;
-       this.state.data = data;
-       //this.forceUpdate()
-      //}
-      //return data;
-    };
-    xhr.send();
-    //return xhr.response;
-  }
-
-
-
-  componentDidMount() {
-     this.loadCommentsFromServer();
-    window.setInterval(() => this.loadCommentsFromServer(), 2001);
-    this.forceUpdate();
-  }
-  render() {
-    return (
-      <div className="commentBox">
-        <h1>Comments</h1>
-        <CommentList data={this.state.data} />
-        <CommentForm />
-      </div>
-    );
-  }
-}
-
 ReactDOM.render(
-  <CommentBox url="/comments" pollInterval={2000} />,
+  <CommentBox url="/comments" pollInterval={2000} data="[]" />,
   document.getElementById('content')
 );
